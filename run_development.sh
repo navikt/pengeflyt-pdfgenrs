2b56f446b581e8f0feff7a679802fcffc2f7256c
Harald Meland <harald.roisland.meland@nav.no>
2026-05-07 15:49:56 +0200

    TOB-6036: La run_development.sh hente riktig docker-container fra Dockerfile

--- run_development.sh
+++ run_development.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+CURRENT_PATH="$(cd "$(dirname "$0")" && pwd)"
+BASEIMG="$(grep '^FROM ' "$CURRENT_PATH/Dockerfile" | head -n 1 | awk '{print $2}')"
 
-docker pull ghcr.io/navikt/pdfgen:2.0.97
+docker pull "$BASEIMG"
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/data:/app/data \
         -v $CURRENT_PATH/resources:/app/resources \
+        -v $CURRENT_PATH/resources:/app/templates/resources \
         -p 8080:8080 \
-        -e DISABLE_PDF_GET=false \
-        -e JDK_JAVA_OPTIONS \
         -e DEV_MODE=true \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgen:2.0.97
+        "$BASEIMG"
